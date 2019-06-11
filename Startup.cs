62209834c74fa3b803ac1bd96cf136d199119dca
alexander.buczynsky <alexander.buczynsky@siemens.com>
2019-06-11 17:43:58 -0500

    add CORS support

--- Startup.cs
+++ Startup.cs
@@ -25,12 +25,20 @@ namespace TodoApi
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("MyPolicy");
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -43,6 +51,8 @@ namespace TodoApi
 
             // app.UseHttpsRedirection();
             app.UseMvc();
+            
+    
         }
     }
 }
