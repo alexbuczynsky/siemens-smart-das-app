65bdbd4330ab59101f84d184c6e26b903ca2944d
Alexander Buczynsky <alexander.buczynsky@siemens.com>
2019-06-12 15:49:19 -0500

    run and build commands updated

--- build-windows.sh
+++ build-windows.sh
@@ -4,12 +4,14 @@ cd server
 
 dotnet restore
 
-dotnet publish -r osx.10.11-x64 --output bin/dist/osx
-dotnet publish -r linux-x64 --output bin/dist/linux
+rm -r bin/dist
+dotnet build
 dotnet publish -r win10-x64 --output bin/dist/win
 
 cd ../client
 
 npm install
+
+npm run build
 
 npm run build:electron:windows
