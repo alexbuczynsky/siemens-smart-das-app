72320a4b30da331078424ec7151ea5f20ffebe1e
alexander.buczynsky <alexander.buczynsky@siemens.com>
2019-06-19 18:09:18 -0500

    Shows status of DAS commands in List

--- client/src/components/ConfiguredBreakers/List.tsx
+++ client/src/components/ConfiguredBreakers/List.tsx
@@ -41,57 +41,92 @@ export const BreakerList: React.FC<BreakerListProps> = props => {
 
   const dasStatuses = useStore(state => state.breakers.DAS.status);
   const alarms = useStore(state => state.breakers.alarms);
+  const activateCommands = useStore(state => state.breakers.DAS.activateCommands)
 
 
-  const getStatus = (id: number) => {
+  type BreakerStatus = {
+    isDASEnabled: boolean;
+    isCOMAlarmActive: boolean;
+    activateCommandState: boolean;
+  }
+  const getBreakerState = (id: number): BreakerStatus => {
+    const status = {
+      isDASEnabled: false,
+      isCOMAlarmActive: false,
+      activateCommandState: false,
+    };
+
     switch (id) {
       case 1:
-        return dasStatuses.dasStatusBreaker1;
+        status.isDASEnabled = dasStatuses.dasStatusBreaker1;
+        status.isCOMAlarmActive = alarms.comAlarmBreaker1;
+        status.activateCommandState = activateCommands.dasActivateBreaker1;
+        break;
       case 2:
-        return dasStatuses.dasStatusBreaker2;
+        status.isDASEnabled = dasStatuses.dasStatusBreaker2;
+        status.isCOMAlarmActive = alarms.comAlarmBreaker2;
+        status.activateCommandState = activateCommands.dasActivateBreaker2;
+        break;
       case 3:
-        return dasStatuses.dasStatusBreaker3;
+        status.isDASEnabled = dasStatuses.dasStatusBreaker3;
+        status.isCOMAlarmActive = alarms.comAlarmBreaker3;
+        status.activateCommandState = activateCommands.dasActivateBreaker3;
+        break;
       case 4:
-        return dasStatuses.dasStatusBreaker4;
+        status.isDASEnabled = dasStatuses.dasStatusBreaker4;
+        status.isCOMAlarmActive = alarms.comAlarmBreaker4;
+        status.activateCommandState = activateCommands.dasActivateBreaker4;
+        break;
       case 5:
-        return dasStatuses.dasStatusBreaker5;
+        status.isDASEnabled = dasStatuses.dasStatusBreaker5;
+        status.isCOMAlarmActive = alarms.comAlarmBreaker5;
+        status.activateCommandState = activateCommands.dasActivateBreaker5;
+        break;
       case 6:
-        return dasStatuses.dasStatusBreaker6;
+        status.isDASEnabled = dasStatuses.dasStatusBreaker6;
+        status.isCOMAlarmActive = alarms.comAlarmBreaker6;
+        status.activateCommandState = activateCommands.dasActivateBreaker6;
+        break;
       case 7:
-        return dasStatuses.dasStatusBreaker7;
+        status.isDASEnabled = dasStatuses.dasStatusBreaker7;
+        status.isCOMAlarmActive = alarms.comAlarmBreaker7;
+        status.activateCommandState = activateCommands.dasActivateBreaker7;
+        break;
       case 8:
-        return dasStatuses.dasStatusBreaker8;
+        status.isDASEnabled = dasStatuses.dasStatusBreaker8;
+        status.isCOMAlarmActive = alarms.comAlarmBreaker8;
+        status.activateCommandState = activateCommands.dasActivateBreaker8;
+        break;
       case 9:
-        return dasStatuses.dasStatusBreaker9;
-      default:
-        return null;
+        status.isDASEnabled = dasStatuses.dasStatusBreaker9;
+        status.isCOMAlarmActive = alarms.comAlarmBreaker9;
+        status.activateCommandState = activateCommands.dasActivateBreaker9;
+        break;
     }
+
+    return status;
   }
 
-  const getAlarmStatus = (id: number) => {
-    switch (id) {
-      case 1:
-        return alarms.comAlarmBreaker1;
-      case 2:
-        return alarms.comAlarmBreaker2;
-      case 3:
-        return alarms.comAlarmBreaker3;
-      case 4:
-        return alarms.comAlarmBreaker4;
-      case 5:
-        return alarms.comAlarmBreaker5;
-      case 6:
-        return alarms.comAlarmBreaker6;
-      case 7:
-        return alarms.comAlarmBreaker7;
-      case 8:
-        return alarms.comAlarmBreaker8;
-      case 9:
-        return alarms.comAlarmBreaker9;
-      default:
-        return null;
-    }
+  const renderTableRow = (breaker: BreakerSetupObject) => {
+    const breakerState = getBreakerState(breaker.id);
+
+    return (
+      <TableRow key={breaker.id}>
+        <TableCell align="left">{breaker.id}</TableCell>
+        <TableCell align="left">{breaker.breakerTypeAsString}</TableCell>
+        <TableCell align="left">{breakerState.isDASEnabled ? "ON" : "OFF"}</TableCell>
+        <TableCell align="left">{breakerState.activateCommandState ? "ON" : "OFF"}</TableCell>
+        <TableCell align="left">{breakerState.isCOMAlarmActive ? "Alarm Active" : "No Alarm"}</TableCell>
+        <TableCell align="left">
+          <ConfigureBreakerButton breaker={breaker} />
+        </TableCell>
+      </TableRow>
+    )
   }
+
+
+
+
 
   return (
     <div className={classes.root}>
@@ -101,24 +136,13 @@ export const BreakerList: React.FC<BreakerListProps> = props => {
             <TableCell align="left">Number</TableCell>
             <TableCell align="left">Type</TableCell>
             <TableCell align="left">DAS State</TableCell>
+            <TableCell align="left">DAS Command State</TableCell>
             <TableCell align="left">Alarm</TableCell>
             <TableCell align="left"></TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {props.breakers.map(breaker => (
-            <TableRow key={breaker.id}>
-              <TableCell align="left">
-                {breaker.id}
-              </TableCell>
-              <TableCell align="left">{breaker.breakerTypeAsString}</TableCell>
-              <TableCell align="left">{getStatus(breaker.id) ? "ON" : "OFF"}</TableCell>
-              <TableCell align="left">{getAlarmStatus(breaker.id) ? "Alarm Active" : "No Alarm"}</TableCell>
-              <TableCell align="left">
-                <ConfigureBreakerButton breaker={breaker} />
-              </TableCell>
-            </TableRow>
-          ))}
+          {props.breakers.map(renderTableRow)}
         </TableBody>
       </Table>
     </div>
