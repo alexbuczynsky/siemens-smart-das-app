117ba03d50acb4441d0fbc802b38a353a3720c4d
alexander.buczynsky <alexander.buczynsky@siemens.com>
2019-06-20 16:22:58 -0500

    created a hook for breaker statuses

--- client/src/components/ConfiguredBreakers/List.tsx
+++ client/src/components/ConfiguredBreakers/List.tsx
@@ -10,6 +10,7 @@ import { useStore } from '../../hooks';
 import { Table, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
 import { BreakerSetupObject } from '../../models';
 import { ConfigureBreakerButton } from './ConfigureBreakerButton';
+import { useBreakerStatuses } from '../../hooks/useBreakerStatus';
 
 // -------------------------------------------------------------------------
 // STYLES
@@ -39,76 +40,14 @@ export type BreakerListProps = {
 export const BreakerList: React.FC<BreakerListProps> = props => {
   const classes = useStyles();
 
-  const dasStatuses = useStore(state => state.breakers.DAS.status);
-  const alarms = useStore(state => state.breakers.alarms);
-  const activateCommands = useStore(state => state.breakers.DAS.activateCommands)
-
-
-  type BreakerStatus = {
-    isDASEnabled: boolean;
-    isCOMAlarmActive: boolean;
-    activateCommandState: boolean;
-  }
-  const getBreakerState = (id: number): BreakerStatus => {
-    const status = {
-      isDASEnabled: false,
-      isCOMAlarmActive: false,
-      activateCommandState: false,
-    };
-
-    switch (id) {
-      case 1:
-        status.isDASEnabled = dasStatuses.dasStatusBreaker1;
-        status.isCOMAlarmActive = alarms.comAlarmBreaker1;
-        status.activateCommandState = activateCommands.dasActivateBreaker1;
-        break;
-      case 2:
-        status.isDASEnabled = dasStatuses.dasStatusBreaker2;
-        status.isCOMAlarmActive = alarms.comAlarmBreaker2;
-        status.activateCommandState = activateCommands.dasActivateBreaker2;
-        break;
-      case 3:
-        status.isDASEnabled = dasStatuses.dasStatusBreaker3;
-        status.isCOMAlarmActive = alarms.comAlarmBreaker3;
-        status.activateCommandState = activateCommands.dasActivateBreaker3;
-        break;
-      case 4:
-        status.isDASEnabled = dasStatuses.dasStatusBreaker4;
-        status.isCOMAlarmActive = alarms.comAlarmBreaker4;
-        status.activateCommandState = activateCommands.dasActivateBreaker4;
-        break;
-      case 5:
-        status.isDASEnabled = dasStatuses.dasStatusBreaker5;
-        status.isCOMAlarmActive = alarms.comAlarmBreaker5;
-        status.activateCommandState = activateCommands.dasActivateBreaker5;
-        break;
-      case 6:
-        status.isDASEnabled = dasStatuses.dasStatusBreaker6;
-        status.isCOMAlarmActive = alarms.comAlarmBreaker6;
-        status.activateCommandState = activateCommands.dasActivateBreaker6;
-        break;
-      case 7:
-        status.isDASEnabled = dasStatuses.dasStatusBreaker7;
-        status.isCOMAlarmActive = alarms.comAlarmBreaker7;
-        status.activateCommandState = activateCommands.dasActivateBreaker7;
-        break;
-      case 8:
-        status.isDASEnabled = dasStatuses.dasStatusBreaker8;
-        status.isCOMAlarmActive = alarms.comAlarmBreaker8;
-        status.activateCommandState = activateCommands.dasActivateBreaker8;
-        break;
-      case 9:
-        status.isDASEnabled = dasStatuses.dasStatusBreaker9;
-        status.isCOMAlarmActive = alarms.comAlarmBreaker9;
-        status.activateCommandState = activateCommands.dasActivateBreaker9;
-        break;
-    }
-
-    return status;
-  }
+  const breakerStatuses = useBreakerStatuses();
 
   const renderTableRow = (breaker: BreakerSetupObject) => {
-    const breakerState = getBreakerState(breaker.id);
+    const breakerState = breakerStatuses.find(x => x.breakerId === breaker.id);
+
+    if (breakerState === undefined) {
+      return null;
+    }
 
     return (
       <TableRow key={breaker.id}>
