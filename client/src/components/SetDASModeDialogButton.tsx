e939eaa983796bfe6c5853ddc55f26cfe52570c9
ALEXANDER BUCZYNSKY <alexander.buczynsky@siemens.com>
2019-06-26 17:57:28 -0500

    set das mode dialog cleaned up

--- client/src/components/SetDASModeDialogButton.tsx
+++ client/src/components/SetDASModeDialogButton.tsx
@@ -3,18 +3,38 @@
 // -------------------------------------------------------------------------
 
 // Import React
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // Material UI Imports
-import { makeStyles } from '@smartgear/edison';
-import { Button, Dialog, DialogTitle, DialogContent, DialogContentText, DialogActions, FormLabel, RadioGroup, FormControlLabel, Radio, List, ListItem, ListItemAvatar, Avatar, ListItemText, ListItemSecondaryAction, IconButton } from '@material-ui/core';
-import { AsyncSaveButton } from './AsyncSaveButton';
-import { useStore } from '../hooks';
-import { SmartDASClientService } from '../services/configured-services';
-import { StoreActions } from '../store';
-import { BreakerSetupObject } from '../models';
-import { ButtonProps } from '@material-ui/core/Button';
-import DeleteIcon from '@material-ui/icons/Delete';
-import { useBreakerStatuses } from '../hooks/useBreakerStatus';
+import { makeStyles } from "@smartgear/edison";
+import {
+  Button,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  List,
+  ListItem,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  ListItemSecondaryAction,
+  IconButton,
+  Badge,
+  Chip
+} from "@material-ui/core";
+import { AsyncSaveButton } from "./AsyncSaveButton";
+import { useStore } from "../hooks";
+import { SmartDASClientService } from "../services/configured-services";
+import { StoreActions } from "../store";
+import { BreakerSetupObject } from "../models";
+import { ButtonProps } from "@material-ui/core/Button";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { useBreakerStatuses } from "../hooks/useBreakerStatus";
 
 // ---------------------------------------  ----------------------------------
 // STYLES
@@ -22,10 +42,16 @@ import { useBreakerStatuses } from '../hooks/useBreakerStatus';
 
 const useStyles = makeStyles(theme => ({
   root: {
-    maxWidth: '100%'
+    maxWidth: "100%"
   },
   list: {
-    minWidth: '500px',
+    minWidth: "500px"
+  },
+  dastStatusIndicatorContainer: {
+    flex: "1 1 auto"
+  },
+  dasStatusIndicator: {
+    margin: "auto"
   }
 }));
 
@@ -33,29 +59,51 @@ const useStyles = makeStyles(theme => ({
 // OPTIONS
 // -------------------------------------------------------------------------
 
-export interface SetDASModeDialogButtonProps extends ButtonProps { };
+export interface SetDASModeDialogButtonProps extends ButtonProps {}
+
+// -------------------------------------------------------------------------
+// Sub Components
+// -------------------------------------------------------------------------
+
+interface DASIndicatorProps {
+  isEnabled: boolean;
+}
+
+const DASIndicatorStatus: React.FC<DASIndicatorProps> = props => {
+  const classes = useStyles();
+
+  const color = props.isEnabled ? "primary" : "default";
+  const label = props.isEnabled ? "DAS is Enabled" : "DAS is Disabled";
+  return (
+    <div className={classes.dastStatusIndicatorContainer}>
+      <Chip color={color} label={label} />
+    </div>
+  );
+};
 
 // -------------------------------------------------------------------------
 // MAIN COMPONENT
 // -------------------------------------------------------------------------
 
-export const SetDASModeDialogButton: React.FC<SetDASModeDialogButtonProps> = props => {
+export const SetDASModeDialogButton: React.FC<
+  SetDASModeDialogButtonProps
+> = props => {
   const classes = useStyles();
 
   const breakers = useStore(state => state.breakers.List);
-  const activateCommands = useStore(state => state.breakers.DAS.activateCommands)
+  const activateCommands = useStore(
+    state => state.breakers.DAS.activateCommands
+  );
   const breakerStatuses = useBreakerStatuses();
   const StoreSiteSwitchType = useStore(state => state.breakers.switchType);
 
   const [open, setOpen] = useState(false);
 
-
   const [switchType, setSwitchType] = useState(StoreSiteSwitchType);
 
-
   useEffect(() => {
-    setSwitchType(StoreSiteSwitchType)
-  }, [StoreSiteSwitchType])
+    setSwitchType(StoreSiteSwitchType);
+  }, [StoreSiteSwitchType]);
 
   function handleClickOpen() {
     setOpen(true);
@@ -67,31 +115,32 @@ export const SetDASModeDialogButton: React.FC<SetDASModeDialogButtonProps> = pro
 
   async function handleSave() {
     try {
-      const siteSetupStructure = BreakerSetupObject.ConvertToSiteSetupStructure(breakers, switchType);
-      const payload = await SmartDASClientService.setSiteSetupStructure(siteSetupStructure);
+      const siteSetupStructure = BreakerSetupObject.ConvertToSiteSetupStructure(
+        breakers,
+        switchType
+      );
+      const payload = await SmartDASClientService.setSiteSetupStructure(
+        siteSetupStructure
+      );
 
       StoreActions.Breakers.setSwitchType(payload.switchType);
-
     } catch (err) {
-
       StoreActions.Notifications.publishError({
         title: `Switch Type Failed updating`,
-        message: err.message,
-      })
+        message: err.message
+      });
 
       return {
         isSuccess: false
-      }
+      };
     }
 
     return {
       isSuccess: true
-    }
+    };
   }
 
-
   function renderListItem(breaker: BreakerSetupObject) {
-
     const breakerState = breakerStatuses.find(x => x.breakerId === breaker.id);
 
     if (breakerState === undefined) {
@@ -100,50 +149,50 @@ export const SetDASModeDialogButton: React.FC<SetDASModeDialogButtonProps> = pro
 
     const handleToggle = async () => {
       const id = breaker.id;
-      (activateCommands as any)[`dasActivateBreaker${id}`] = !breakerState.activateCommandState;
+      (activateCommands as any)[
+        `dasActivateBreaker${id}`
+      ] = !breakerState.activateCommandState;
       try {
-        const payload = await SmartDASClientService.setDASActiveCommands(activateCommands);
+        const payload = await SmartDASClientService.setDASActiveCommands(
+          activateCommands
+        );
         StoreActions.Breakers.updateDASCommands(payload);
-      } catch (e) {
-
-      }
-
-    }
+      } catch (e) {}
+    };
 
     return (
       <ListItem key={breaker.id}>
-        <ListItemText
-          primary={`Breaker ${breaker.id}`}
+        <ListItemText primary={`Breaker ${breaker.id}`} />
+        <DASIndicatorStatus isEnabled={breakerState.isDASEnabled} />
 
-        />
-        <ListItemText
-          primary={breakerState.isDASEnabled ? "DAS is Enabled" : "DAS is Disabled"}
-        />
-        <ListItemText
-          primary={breakerState.activateCommandState ? "PLC is Enabling DAS" : "PLC is Disabling DAS"}
-        />
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={handleToggle}
-        >
-          {breakerState.activateCommandState ? "Turn on DAS" : "Turn off DAS"}
+        <Button variant="contained" color="primary" onClick={handleToggle}>
+          {breakerState.isDASEnabled ? "Turn off DAS" : "Turn on DAS"}
         </Button>
       </ListItem>
-    )
+    );
   }
 
   return (
     <div>
-      <Button {...props} variant="outlined" color="primary" onClick={handleClickOpen}>
+      <Button
+        {...props}
+        variant="outlined"
+        color="primary"
+        onClick={handleClickOpen}
+      >
         Set Breaker DAS Modes
       </Button>
-      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="form-dialog-title"
+      >
         <DialogTitle id="form-dialog-title">Set Breaker DAS Modes</DialogTitle>
         <DialogContent>
           <DialogContentText>
-            Modify the DAS state by using the interface below.
-            To enable / disable DAS mode press the button on the right next to the breaker you are testing.
+            Modify the DAS state by using the interface below. To enable /
+            disable DAS mode press the button on the right next to the breaker
+            you are testing.
           </DialogContentText>
           <List dense className={classes.list}>
             {breakers.map(renderListItem)}
@@ -156,5 +205,5 @@ export const SetDASModeDialogButton: React.FC<SetDASModeDialogButtonProps> = pro
         </DialogActions>
       </Dialog>
     </div>
-  )
+  );
 };
