50fb21bc5471710e7048249cd38ec5fc90aafce3
ALEXANDER BUCZYNSKY <alexander.buczynsky@siemens.com>
2019-06-26 17:58:59 -0500

    all api requests are synchronous

--- client/src/views/DASDashboard.tsx
+++ client/src/views/DASDashboard.tsx
@@ -3,29 +3,29 @@
 // -------------------------------------------------------------------------
 
 // Import React
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 // Material UI Imports
-import { makeStyles } from '@smartgear/edison';
-import { Grid } from '@material-ui/core';
-import { BreakerSetupObject } from '../models';
-import { SmartDASClientService } from '../services/configured-services';
-import { ConnectionStatusCard } from '../components/PLCConnectionStatusCard';
-import { ConfiguredBreakersCard } from '../components/ConfiguredBreakers/Card';
-import { useStore, useInterval } from '../hooks';
-import Store, { StoreActions } from '../store';
-import { ToolsCard } from '../components/ToolsCard';
-import { SiteSetupStructure } from '../models/SiteSetupStructure';
+import { makeStyles } from "@smartgear/edison";
+import { Grid } from "@material-ui/core";
+import { BreakerSetupObject } from "../models";
+import { SmartDASClientService } from "../services/configured-services";
+import { ConnectionStatusCard } from "../components/PLCConnectionStatusCard";
+import { ConfiguredBreakersCard } from "../components/ConfiguredBreakers/Card";
+import { useStore, useInterval } from "../hooks";
+import Store, { StoreActions } from "../store";
+import { ToolsCard } from "../components/ToolsCard";
+import { SiteSetupStructure } from "../models/SiteSetupStructure";
 
 // -------------------------------------------------------------------------
 // STYLES
 // -------------------------------------------------------------------------
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(theme => ({
   root: {
-    margin: 'auto',
-    width: '90%',
-    maxWidth: '1920px',
-  },
+    margin: "auto",
+    width: "90%",
+    maxWidth: "1920px"
+  }
 }));
 
 // -------------------------------------------------------------------------
@@ -33,6 +33,9 @@ const useStyles = makeStyles((theme) => ({
 // -------------------------------------------------------------------------
 
 export type DASDashboardProps = {};
+
+const STATUS_FETCH_INTERVAL = 2000;
+const CONNECTION_TEST_INTERVAL = 3000;
 
 // -------------------------------------------------------------------------
 // MAIN COMPONENT
@@ -43,56 +46,65 @@ export const DASDashboard: React.FC<DASDashboardProps> = props => {
 
   const PLCIsConnected = useStore(state => state.breakers.isPLCConnected);
 
-  const [fetchStatusInterval, setFetchStatusInterval] = useState(2000);
+  const [fetchStatusInterval, setFetchStatusInterval] = useState(
+    STATUS_FETCH_INTERVAL
+  );
+  const [checkConnectionInterval, setCheckConnectionInterval] = useState(
+    CONNECTION_TEST_INTERVAL
+  );
+
+  // useEffect(() => {
+  //   SmartDASClientService
+  //     .getSiteSetupStructure()
+  //     .then(config => {
+  //       const setupStructure = new SiteSetupStructure(config);
+
+  //       StoreActions.Breakers.updateAll(setupStructure.breakers);
+  //       StoreActions.Breakers.setSwitchType(setupStructure.switchType);
+  //     })
+  //     .catch(err => console.error(err))
+  // }, [PLCIsConnected])
+
+  // CHECK PLC CONNECTION STATUS
+  useInterval(() => {
+    StoreActions.TestPLCConnection().catch(err => console.error(err));
+  }, checkConnectionInterval);
 
   useEffect(() => {
-    SmartDASClientService
-      .getSiteSetupStructure()
+    if (PLCIsConnected) {
+      setFetchStatusInterval(STATUS_FETCH_INTERVAL);
+      setCheckConnectionInterval(10000);
+    } else {
+      setFetchStatusInterval(0);
+      setCheckConnectionInterval(CONNECTION_TEST_INTERVAL);
+    }
+  }, [PLCIsConnected]);
+
+  useInterval(() => {
+    StoreActions.TestPLCConnection()
+      .then(() => SmartDASClientService.getSiteSetupStructure())
       .then(config => {
         const setupStructure = new SiteSetupStructure(config);
 
         StoreActions.Breakers.updateAll(setupStructure.breakers);
         StoreActions.Breakers.setSwitchType(setupStructure.switchType);
       })
-      .catch(err => console.error(err))
-  }, [PLCIsConnected])
-
-
-  // CHECK PLC CONNECTION STATUS
-  useInterval(() => {
-    StoreActions.TestPLCConnection();
-  }, 3000)
-
-  useEffect(() => {
-    if (PLCIsConnected) {
-      setFetchStatusInterval(2000);
-    } else {
-      setFetchStatusInterval(0);
-    }
-  }, [PLCIsConnected])
-
-
-  useInterval(() => {
-    SmartDASClientService
-      .getDASStatus()
+      .then(() => SmartDASClientService.getDASStatus())
       .then(status => {
         StoreActions.Breakers.updateDASStatus(status);
+        return;
       })
-
-    SmartDASClientService
-      .getActiveAlarms()
+      .then(() => SmartDASClientService.getActiveAlarms())
       .then(alarms => {
         StoreActions.Breakers.updateAlarms(alarms);
+        return;
       })
-
-    SmartDASClientService
-      .getDASActiveCommands()
+      .then(() => SmartDASClientService.getDASActiveCommands())
       .then(commands => {
         StoreActions.Breakers.updateDASCommands(commands);
       })
-  }, fetchStatusInterval)
-
-
+      .catch(err => console.error(err));
+  }, fetchStatusInterval);
 
   return (
     <div className={classes.root}>
@@ -107,6 +119,6 @@ export const DASDashboard: React.FC<DASDashboardProps> = props => {
           <ConfiguredBreakersCard />
         </Grid>
       </Grid>
-    </div >
+    </div>
   );
 };
