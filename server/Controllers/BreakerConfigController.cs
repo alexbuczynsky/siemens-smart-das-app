740de1756a80e2282e9b0f15d7ffd30071b29450
Alexander Buczynsky <alexander.buczynsky@siemens.com>
2019-06-28 00:06:32 -0500

    removed BreakerConfigAPI.Database

--- server/Controllers/BreakerConfigController.cs
+++ server/Controllers/BreakerConfigController.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BreakerConfigAPI.Database;
+
 using BreakerConfigAPI.Models;
 using Microsoft.AspNetCore.Mvc;
 using smartDASNamespace;
